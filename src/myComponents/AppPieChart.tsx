f8f6b46715465bb2e831fdfde7a596aa12b8dc09
Saikh Seraj Fahim <saikhseraj2017@gmail.com>
2025-11-14 00:45:47 +0600

    piechart

--- src/myComponents/AppPieChart.tsx
+++ src/myComponents/AppPieChart.tsx
@@ -1,11 +1,10 @@
 // Pie Chart - Donut with Text
 
-import { ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent, type ChartConfig }
-    from "../components/ui/chart"
+import { ChartTooltip, ChartTooltipContent, type ChartConfig }
+    from "../components/ui/chart";
 import { ChartContainer } from "../components/ui/chart";
-import { Area, AreaChart, CartesianGrid, Pie, PieChart, XAxis, YAxis } from "recharts";
-import { Label } from "../components/ui/label";
-import { CardContent, CardFooter } from "../components/ui/card";
+import { Pie, PieChart, Label } from "recharts";
+import { Card, CardContent, CardFooter } from "../components/ui/card";
 import { TrendingUp } from "lucide-react";
 
 const chartData = [
@@ -40,7 +39,8 @@ const chartConfig = {
         label: "Other",
         color: "var(--chart-5)",
     },
-} satisfies ChartConfig
+} satisfies ChartConfig;
+
 
 export default function AppPieChart() {
     const totalVisitors = chartData.reduce((acc, curr) => acc + curr.visitors, 0);
@@ -48,62 +48,64 @@ export default function AppPieChart() {
     return (
         <div>
             <h1 className="font-medium text-lg mb-4">Browser Usage</h1>
-            <CardContent className="flex-1 pb-0">
-                <ChartContainer config={chartConfig} className="mx-auto aspect-square max-h-[250px]">
-                    <PieChart>
-                        <ChartTooltip
-                            cursor={false}
-                            content={<ChartTooltipContent hideLabel />}
-                        />
-                        <Pie
-                            data={chartData}
-                            dataKey="visitors"
-                            nameKey="browser"
-                            innerRadius={60}
-                            strokeWidth={5}
-                        >
-                            <Label
-                                content={({ viewBox }) => {
-                                    if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                                        return (
-                                            <text
-                                                x={viewBox.cx}
-                                                y={viewBox.cy}
-                                                textAnchor="middle"
-                                                dominantBaseline="middle"
-                                            >
-                                                <tspan
+            <Card className="flex flex-col">
+                <CardContent className="flex-1 pb-0">
+                    <ChartContainer config={chartConfig} className="mx-auto aspect-square max-h-[250px]">
+                        <PieChart>
+                            <ChartTooltip
+                                cursor={false}
+                                content={<ChartTooltipContent hideLabel />}
+                            />
+                            <Pie
+                                data={chartData}
+                                dataKey="visitors"
+                                nameKey="browser"
+                                innerRadius={60}
+                                strokeWidth={5}
+                            >
+                                <Label
+                                    content={({ viewBox }) => {
+                                        if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                                            return (
+                                                <text
                                                     x={viewBox.cx}
                                                     y={viewBox.cy}
-                                                    className="fill-foreground text-3xl font-bold"
+                                                    textAnchor="middle"
+                                                    dominantBaseline="middle"
                                                 >
-                                                    {totalVisitors.toLocaleString()}
-                                                </tspan>
-                                                <tspan
-                                                    x={viewBox.cx}
-                                                    y={(viewBox.cy || 0) + 24}
-                                                    className="fill-muted-foreground"
-                                                >
-                                                    Visitors
-                                                </tspan>
-                                            </text>
-                                        )
-                                    }
-                                }}
-                            />
-                        </Pie>
-                    </PieChart>
-                </ChartContainer>
-            </CardContent>
+                                                    <tspan
+                                                        x={viewBox.cx}
+                                                        y={viewBox.cy}
+                                                        className="fill-foreground text-3xl font-bold"
+                                                    >
+                                                        {totalVisitors.toLocaleString()}
+                                                    </tspan>
+                                                    <tspan
+                                                        x={viewBox.cx}
+                                                        y={(viewBox.cy || 0) + 24}
+                                                        className="fill-muted-foreground text-base"
+                                                    >
+                                                        Visitors
+                                                    </tspan>
+                                                </text>
+                                            )
+                                        }
+                                    }}
+                                />
+                            </Pie>
+                        </PieChart>
+                    </ChartContainer>
+                </CardContent>
 
-            <CardFooter className="flex-col gap-2 text-sm">
-                <div className="flex items-center gap-2 leading-none font-medium">
-                    Trending up by 5.2% this month <TrendingUp className="h-4 w-4 text-green-500" />
-                </div>
-                <div className="text-muted-foreground leading-none">
-                    Showing total visitors for the last 6 months
-                </div>
-            </CardFooter>
+                <CardFooter className="flex-col gap-2 text-sm">
+                    <div className="flex items-center gap-2 leading-none font-medium">
+                        Trending up by 5.2% this month <TrendingUp className="h-4 w-4 text-green-500" />
+                    </div>
+                    <div className="text-muted-foreground leading-none">
+                        Showing total visitors for the last 6 months
+                    </div>
+                </CardFooter>
+            </Card>
         </div>
     );
 }
