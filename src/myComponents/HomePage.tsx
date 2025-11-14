9088df86c1ade62515cc188c468f1110a7af3a5e
Saikh Seraj Fahim <saikhseraj2017@gmail.com>
2025-11-14 07:35:30 +0600

    up-2

--- src/myComponents/HomePage.tsx
+++ src/myComponents/HomePage.tsx
@@ -1,15 +1,17 @@
 import AppAreaChart from "./AppAreaChart";
 import AppBarChart from "./AppBarChart";
 import AppPieChart from "./AppPieChart";
+import CardList from "./CardList";
+import ToDoList from "./ToDoList";
 
 export default function HomePage() {
     return (
         <div>
             <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-6">
                 <div className="bg-primary-foreground p-4 rounded-lg"><AppBarChart /></div>
-                <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+                <div className="bg-primary-foreground p-4 rounded-lg"><CardList title="Latest Transactions" /></div>
                 <div className="bg-primary-foreground p-4 rounded-lg"><AppPieChart /></div>
-                <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+                <div className="bg-primary-foreground p-4 rounded-lg"><ToDoList /></div>
                 <div className="bg-primary-foreground p-4 rounded-lg"><AppAreaChart /></div>
                 <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
 
